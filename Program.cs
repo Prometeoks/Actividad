2369c1940f05b6511036b8a70e8593883ab77999
MathiasP59 <mathiaslin12@gmail.com>
2023-05-18 23:20:33 -0500

    agregue una linea que indica que el programa cerrara pronto

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ namespace Actividad
                     break;
             }
             Console.WriteLine("El resultado de la operación es: " + resultado);
+            Console.WriteLine("----- EL PROGRAMA FINALIZARA... -----");
             Console.ReadLine();
         }
     }
